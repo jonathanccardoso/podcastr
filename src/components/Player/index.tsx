1d529df71f710467c3974e9b7d75416d79c9d2e1
jonathanccardoso <jonathan99moura@hotmail.com>
2021-04-28 16:17:48 -0300

    Fixme: adds component Player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,20 +1,45 @@
-import format from "date-fns/format";
-import ptBR from "date-fns/locale/pt-BR";
-
 import styles from "./styles.module.scss";
 
-export function Header() {
-  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
-    locale: ptBR,
-  });
-
+export function Player() {
   return (
-    <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr" />
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando agora" />
+        <strong>Tocando agora</strong>
+      </header>
 
-      <p>O melhor para você ouvir, sempre</p>
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
 
-      <span>{currentDate}</span>
-    </header>
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlider} />
+          </div>
+          <span>00:00</span>
+        </div>
+
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffer.svg" alt="Embaralhar" />
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="Tocar anterior" />
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="Tocar" />
+          </button>
+
+          <button type="button">
+            <img src="/play-next.svg" alt="Tocar próxima" />
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="Repetir" />
+          </button>
+        </div>
+      </footer>
+    </div>
   );
 }
